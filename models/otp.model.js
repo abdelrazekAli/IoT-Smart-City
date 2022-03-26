134cd24e531bfcff5dcc977bddcd0260b9c8f5e3
abdelrazekAli <dev.abdelrazek@gmail.com>
2022-03-26 12:44:11 +0200

    Add verify email

---
+++ models/otp.model.js
@@ -0,0 +1,54 @@
+const { DB_URL } = process.env;
+const mongoose = require("mongoose");
+const connectOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
+const otpSchema = mongoose.Schema({
+  email: String,
+  otp: String,
+});
+
+const Otp = mongoose.model("otp", otpSchema);
+
+exports.createNewOtp = async (otp, email) => {
+  try {
+    await mongoose.connect(DB_URL, connectOptions);
+
+    let newOtp = new Otp({
+      email: email,
+      otp: otp,
+    });
+
+    let result = await newOtp.save();
+    mongoose.disconnect();
+    return result;
+  } catch (err) {
+    mongoose.disconnect();
+    throw new Error(err);
+  }
+};
+
+exports.checkOtp = async (email, otp) => {
+  try {
+    await mongoose.connect(DB_URL, connectOptions);
+
+    let result = await Otp.findOne({ email, otp });
+    mongoose.disconnect();
+    return result;
+  } catch (err) {
+    mongoose.disconnect();
+    throw new Error(err);
+  }
+};
+
+exports.deleteOtp = async (email) => {
+  try {
+    await mongoose.connect(DB_URL, connectOptions);
+
+    let result = await Otp.deleteMany({ email: email });
+    mongoose.disconnect();
+    return result;
+  } catch (err) {
+    mongoose.disconnect();
+    throw new Error(err);
+  }
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -6,14 +6,18 @@ const {
   getUser,
   createNewUser,
   login,
+  sendOtp,
   updateUser,
+  verifyEmail,
   changePassword,
 } = require("../controllers/user.controller");
 
 router.get("/:id", getUser);
-router.post("/register", createNewUser);
 router.post("/login", login);
-router.put("/password-change/:id", auth, changePassword);
+router.post("/register", createNewUser);
+router.post("/send-otp", sendOtp);
+router.post("/verify-email", verifyEmail);
 router.put("/:id", auth, updateUser);
+router.put("/password-change/:id", auth, changePassword);
 
 module.exports = router;
